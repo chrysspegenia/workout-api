e0639107e601dc9b75de4306e630c67dbdaafbd8
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-26 11:41:04 +0800

    Add name to users database

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
       sessions: 'users/sessions',
       registrations: 'users/registrations'
     }
-  end
-
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
