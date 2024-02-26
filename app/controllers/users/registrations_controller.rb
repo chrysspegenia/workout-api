412f72b910e8f790367727d20369cbaa2ac74415
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-26 14:44:22 +0800

    Add expose :Athorization condition in cors.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
-  def respond_with(current_user, _ops = {})
+  def respond_with(current_user, _opts = {})
     if resource.persisted?
       render json: {
         status: {code: 200, message: 'Signed up successfully.'},
@@ -14,7 +14,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       }
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{current_user.error.full_messages.to_sentence}"}
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
       }, status: :unprocessable_entity
     end
   end
