4bc6fe4e2163b1ca7a492049d2c888b7bf54564a
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-26 21:55:24 +0800

    Add resources categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -9,6 +11,15 @@ Rails.application.routes.draw do
       registrations: 'users/registrations'
     }
 
+
+  namespace :api do
+    namespace :v1 do
+      resources :categories
+
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
