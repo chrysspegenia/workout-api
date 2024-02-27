d0ef6a1f5c49083469ce7235a24396de192979e7
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-02-27 16:19:38 +0800

    Add resources tasks

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories
-
+      resources :tasks do
+        member do
+          patch :completed
+        end
+      end
     end
   end
 
