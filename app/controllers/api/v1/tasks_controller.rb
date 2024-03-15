1b474f21ed2cad12d1288e98ce251f09945cb0f8
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-03-15 19:28:07 +0800

    Remove authenticate_user

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,6 @@
 module Api
     module V1
         class TasksController <ApplicationController
-            before_action :authenticate_user!
             before_action :set_task, only: [ :show, :update, :destroy, :complete ]
 
             def index
